2559eeca41db4dc66c7edc43712b12197920e90e
gihanviduranga <gihanvidu123@gmai.com>
2025-06-19 16:32:40 +0530

    User Creation And add Authentication

---
+++ User-Auth-Service/src/main/java/lk/ijse/userauthservice/entity/User.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025
+ * Gihan Viduranga - 2025
+ */
+
+package lk.ijse.userauthservice.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String role;
+}
