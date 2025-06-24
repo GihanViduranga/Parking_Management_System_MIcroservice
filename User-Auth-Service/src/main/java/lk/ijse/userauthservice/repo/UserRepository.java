e2c512119baec299011f4c0b382f4be550a933b0
gihanviduranga <gihanvidu123@gmai.com>
2025-06-25 00:39:10 +0530

    Update User Repo

--- User-Auth-Service/src/main/java/lk/ijse/userauthservice/repo/UserRepository.java
+++ User-Auth-Service/src/main/java/lk/ijse/userauthservice/repo/UserRepository.java
@@ -6,9 +6,11 @@
 package lk.ijse.userauthservice.repo;
 
 
+import lk.ijse.userauthservice.entity.BookingLog;
 import lk.ijse.userauthservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,String> {
